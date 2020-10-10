018ae3470cc5ff5a094ac3634b95cd6bd85492ed
shaw0152040762079 <shaw0152@algonquinlive.com>
2020-10-10 15:27:43 -0400

    f

--- matplot/Reddit.py
+++ matplot/Reddit.py
@@ -52,10 +52,25 @@ def geturlcopypasta():
 
 
 def getartUrls():
+    artUrls: Dict[Union[int, Any], Union[str, Any]] = {0: 'Portraitart',
+                                                       1: 'Art',
+                                                       2: 'painting',
+                                                       3: 'PixelArt',
+                                                       4: 'ArtHistory',
+                                                       5: 'oilpainting',
+                                                       6: 'DigitalPainting',
+                                                       7: 'Watercolor',
+                                                       8: 'low_poly',
+                                                       9: 'VaporwaveAesthetics',
+                                                       10: 'Gouache'}
     return artUrls
 
 
 def getmemeUrls():
+    memeUrls = {0: 'dankmemes',
+                1: 'plantmemes',
+                2: 'chasersriseup',
+                3: 'comedyheaven'}
     return memeUrls
 
 
@@ -65,20 +80,20 @@ def copypasta():
     posts = [post for post in sub.hot(limit=20)]
     random_post_number = random.randint(0, 19)
     random_post = posts[random_post_number]
-    self.posturl = random_post.url
     return random_post.selftext
 
 
 def redditimage(urlKeytoValue, urlnum):
+    reddit = praw.Reddit(client_id=os.environ['client_id'], client_secret=os.environ['client_secret'],
+                         user_agent=os.environ['user_agent'])
     urlkey = random.randint(0, urlnum)
     # create random number to decide which image pulled from array
     url = (urlKeytoValue[urlkey])
-    sub = self.reddit.subreddit(url)
+    sub = reddit.subreddit(url)
 
     posts = [post for post in sub.hot(limit=20)]
     random_post_number = random.randint(0, 19)
     random_post = posts[random_post_number]
-    posturl = random_post.url
     return random_post.url
 
 
--- matplot/views.py
+++ matplot/views.py
@@ -70,70 +70,72 @@ def add(request):
     return render(request, 'result.html', {'data2': uri})
 
 
-self.arts = ''
 
 
 def art(request):
     from matplot import Reddit
     reddit = Reddit.Reddit()
+    arts = ''
+
     urls = []
 
     for i in range(4):
-        url1 = str((Reddit.redditimage(reddit.artUrls, len(reddit.artUrls) - 1)))
+        url1 = str((Reddit.redditimage(Reddit.getartUrls(), len(Reddit.getartUrls()) - 1)))
         data_type = url1[-4:]
 
         while '.jpg' != data_type and '.png' != data_type and '.gif' != data_type:
-            url1 = str((Reddit.redditimage(reddit.artUrls, len(reddit.artUrls) - 1)))
+            url1 = str((Reddit.redditimage(Reddit.getartUrls(), len(Reddit.getartUrls()) - 1)))
             data_type = url1[-4:]
 
         urls += "<ol> <img src =  '" + str(url1) + " ' alt='no image' height '600' width= '465'> </ol> "
 
     for i in range(len(urls)):
-            self.arts += urls[i]
+            arts += urls[i]
 
-    return render(request, 'art.html', {'art': self.arts})
+    return render(request, 'art.html', {'art': arts})
 
 
-self.memes = ''
 
 
 def meme(request):
     from matplot import Reddit
     reddit = Reddit.Reddit()
+    memes = ''
     urls = []
 
     for i in range(4):
-        url1 = str((Reddit.redditimage(reddit.memeUrls, len(reddit.memeUrls) - 1)))
+        url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
         data_type = url1[-4:]
 
         while '.jpg' != data_type and '.png' != data_type and '.gif' != data_type:
             print(url1)
-            url1 = str((Reddit.redditimage(reddit.memeUrls, len(reddit.memeUrls) - 1)))
+            url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
             data_type = url1[-4:]
 
         urls += "<li> <img src =  '" + str(url1) + " ' alt='no image' height '600' width= '465'> </li> "
 
     for i in range(len(urls)):
-        self.memes += urls[i]
+        memes += urls[i]
 
-    return render(request, 'meme.html', {'memes': self.memes})
+    return render(request, 'meme.html', {'memes': memes})
 
 
 # Create your views here.
-self.ensemble = ''
+
 
 
 def story(request):
     from matplot import Reddit
     reddit = Reddit
+    ensemble = ''
     stories = []
     _story = "<li>" + str(reddit.copypasta()) + "</li> <br>"
     stories += _story
 
     for i in range(len(stories)):
-        self.ensemble += stories[i]
+        ensemble += stories[i]
 
-    return render(request, 'story.html', {'story': self.ensemble})
+    return render(request, 'story.html', {'story': ensemble})
 
 
 def definitions(request):
