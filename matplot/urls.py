e8b4acc081196ae0adcdcd72af52d3049c53b949
shaw0152040762079 <shaw0152@algonquinlive.com>
2020-10-24 13:43:08 -0400

    New Speed mods

--- matplot/urls.py
+++ matplot/urls.py
@@ -12,8 +12,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
                   path('', views.home, name='home'),
                   path('add', views.add, name='add'),
-                  path('meme', views.meme, name='meme'),
-                  path('story', views.story, name='story'),
                   path('art', views.art, name='art'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- matplot/views.py
+++ matplot/views.py
@@ -22,17 +22,6 @@ def art(request):
 
     arts = request.session.get('arts', '')
     urls = []
-    if arts == '':
-        posts = Reddit.redditimager(Reddit.getartUrls(), len(Reddit.getartUrls()) - 1)
-        for i in range(6):
-            data_type = posts[i].url[-4:]
-            # Check to make sure they are images
-            if '.jpg' == data_type or '.png' == data_type or '.gif' == data_type:
-                urls += "<li> <img src =  '" + str(posts[i].url) + " ' alt='no image' height '600' width= '465'> </li> "
-        # load all of the images to an HTML list
-        # Add them to the arts list
-        for i in range(len(urls)):
-            arts += urls[i]
 
     # Get random images from reddit related to art
     posts = Reddit.redditimager(Reddit.getartUrls(), len(Reddit.getartUrls()) - 1)
@@ -52,61 +41,4 @@ def art(request):
     return render(request, 'art.html', {'art': arts})
 
 
-def meme(request):
-    from matplot import Reddit
-    # Check to see if previous memes object exists from last session
-    memes = request.session.get('memes', '')
 
-    urls = []
-    if memes == '':
-        for i in range(8):
-            url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
-            data_type = url1[-4:]
-            # Check to make sure they are images
-            while '.jpg' != data_type and '.png' != data_type and '.gif' != data_type:
-                url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
-                data_type = url1[-4:]
-            # load all of the images to an HTML list
-            urls += "<li> <img src =  '" + str(url1) + " ' alt='no image' height '600' width= '465'> </li> "
-        # Add them to the arts list
-        for i in range(len(urls)):
-            memes += urls[i]
-
-    # Get random images from reddit related to art
-    for i in range(3):
-        url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
-        data_type = url1[-4:]
-        # Check to make sure they are images
-        while '.jpg' != data_type and '.png' != data_type and '.gif' != data_type:
-            url1 = str((Reddit.redditimage(Reddit.getmemeUrls(), len(Reddit.getmemeUrls()) - 1)))
-            data_type = url1[-4:]
-        # load all of the images to an HTML list
-        urls += "<li> <img src =  '" + str(url1) + " ' alt='no image' height '600' width= '465'> </li> "
-    # Add them to the arts list
-    for i in range(len(urls)):
-        memes += urls[i]
-    request.session['memes'] = memes
-    return render(request, 'meme.html', {'memes': memes})
-
-
-# Create your views here.
-
-
-def story(request):
-    from matplot import Reddit
-    reddit = Reddit
-    if request.GET.get('Bored? Click to clear cache') == 'Bored? Click to clear cache':
-        request.session['arts'] = ''
-        request.session.modified = True
-    ensemble = request.session.get('ensemble', '')
-
-
-    stories = []
-    _story = "<li>" + str(Reddit.copypasta()) + "</li> <br>"
-    stories += _story
-
-    for i in range(len(stories)):
-        ensemble += stories[i]
-    request.session['ensemble'] = ensemble
-
-    return render(request, 'story.html', {'story': ensemble})
